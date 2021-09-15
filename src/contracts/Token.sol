c5360e6af8679832cde32dcd8acb6ef56fe27d2d
Jen Yang Koh <jenyang@ualberta.ca>
2021-09-14 22:01:13 -0600

    Token information

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,4 +2,11 @@ pragma solidity ^0.5.0;
 
 contract Token {
     string public name = "kamikaze";
+    string public symbol = "KKZ";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10**decimals);
+    }
 }
