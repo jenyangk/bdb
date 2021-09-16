42859b1cc558dd96629d64186e6437a7d351e4d2
Jen Yang Koh <jenyang@ualberta.ca>
2021-09-15 23:05:36 -0600

    Depositing tokens and ether

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -4,14 +4,55 @@
 
 pragma solidity ^0.5.0;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./Token.sol";
+
 contract Exchange {
+    using SafeMath for uint256;
+
     // The account that receives exchange fees
     address public feeAccount;
     // Fee percentage charged for trading
     uint256 public feePercent;
 
+    // Store Ether in tokens mapping with blank address
+    address constant ETHER = address(0);
+
+    // Token address -> User that deposited -> Amount
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    // Events
+    event Deposit(
+        address indexed token,
+        address indexed user,
+        uint256 amount,
+        uint256 balance
+    );
+
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    function() external {
+        revert();
+    }
+
+    function depositEther() public payable {
+        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
+        // Emit event
+        emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+    }
+
+    // Must approve token before you can deposit
+    function depositToken(address _token, uint256 _amount) public {
+        // Don't allow Ether deposits
+        require(_token != ETHER);
+        // Send token to this contract
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        // Manage deposit
+        tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
+        // Emit event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 }
--- test/helpers.js
+++ test/helpers.js
@@ -1,11 +1,16 @@
 const Web3Utils = require("web3-utils");
 
-export const tokens = (n) => {
+export const ether = (n) => {
   return new Web3Utils.toBN(Web3Utils.toWei(n.toString(), "ether"));
 };
+
+// Same as ether
+export const tokens = (n) => ether(n);
 
 export const EVM_REVERT =
   "Returned error: VM Exception while processing transaction: revert";
 
 export const EVM_INVALID_ADDRESS =
   'invalid address (arg="_to", coderType="address", value=0)';
+
+export const ETHER_ADDRESS = "0x0000000000000000000000000000000000000000";
